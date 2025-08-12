bf8465d5a81826dcbad6af5fd08922214a412942
abhishekjaswal <abhishekjaswal1122@gmail.com>
2025-08-12 13:08:08 +0530

    created files and added Configuration for PostgreSQL connection on db.js in the config folder

---
+++ doctor-booking-backend/src/config/db.js
@@ -0,0 +1,12 @@
+// Configuration for PostgreSQL connection pool
+
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default pool;
