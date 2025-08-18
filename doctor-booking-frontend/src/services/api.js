16acf9e4f1fcfd926e82113654f645d64740ee7f
Abhignite Jaswal <36696599+Abhishek-jaswal@users.noreply.github.com>
2025-08-18 18:04:44 +0530

    Update api.js

--- doctor-booking-frontend/src/services/api.js
+++ doctor-booking-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: process.env.REACT_APP_API_URL, 
+ baseURL: https://doctor-booking-dbt7.onrender.com, 
 });
 
 api.interceptors.request.use(config => {
